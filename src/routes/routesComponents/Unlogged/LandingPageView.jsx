8e2cee53623b66222c8add4a40543703f74b6caa
Francisco Javier Rivera <mierderthekat@hotmail.com>
2023-09-17 17:03:23 -0300

    fix: delete unused context

--- src/routes/routesComponents/Unlogged/LandingPageView.jsx
+++ src/routes/routesComponents/Unlogged/LandingPageView.jsx
@@ -1,4 +1,3 @@
-import { LandingPageContextProvider } from "../../../context/Unlogged/LandingPageContext";
 import AnimatedTransition from "../../AnimatedTransition";
 import LandingPage from "../../../components/Unlogged/LandingPage/LandingPage";
 import { Helmet } from "react-helmet";
@@ -6,12 +5,10 @@ import { Helmet } from "react-helmet";
 function LandingPageView() {
   return (
     <AnimatedTransition>
-      <LandingPageContextProvider>
-        <Helmet>
-          <title>Blocksidian | LandingPage</title>
-        </Helmet>
-        <LandingPage />
-      </LandingPageContextProvider>
+      <Helmet>
+        <title>Blocksidian | LandingPage</title>
+      </Helmet>
+      <LandingPage />
     </AnimatedTransition>
   );
 }
